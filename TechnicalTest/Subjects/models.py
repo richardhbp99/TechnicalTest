39afee6102174d5fcef7754d583681b2e3c04878
richardhbp99 <richardbejarano29@gmail.com>
2024-08-01 17:14:12 -0500

    feat: add models Enrollment and Subject

--- TechnicalTest/Subjects/models.py
+++ TechnicalTest/Subjects/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+class Subject(models.Model):
+    id_subject = models.AutoField(primary_key=True, editable=False)
+    code = models.CharField(max_length=10, unique=True) 
+    name = models.CharField(max_length=100)
+    teacher = models.ForeignKey('Teachers.Teacher', on_delete=models.CASCADE)
+    prerequisites = models.ManyToManyField('self', symmetrical=False, related_name='prerequisite_subjects', blank=True)
+
+    def __str__(self):
+        return f"{self.code} - {self.name}"
+    
+    class Meta:
+        db_table = 'T401Subject'
+        verbose_name = 'Subject'
+        verbose_name_plural = 'Subjects'
+
+
+class Enrollment(models.Model):
+    id_enrollment = models.AutoField(primary_key=True, editable=False)
+    student = models.ForeignKey('Students.Student', on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    date = models.DateField()
+
+
+    class Meta:
+        db_table = 'T402Enrollment' 
+        verbose_name = 'Enrollment'
+        verbose_name_plural = 'Enrollments'
+
+    def __str__(self):
+        return f"{self.student.name} - {self.subject.name}"
